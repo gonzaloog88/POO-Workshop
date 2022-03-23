c4ebfea7e461ef55501a7160fab5741050ed7436
gonzaloog88 <gonzaloog88@gmail.com>
2022-03-23 01:56:18 -0600

    POO-WorkShop Finished

---
+++ POO-Workshop/POO-Workshop/ComposedProduct.cs
@@ -0,0 +1,31 @@
+﻿namespace POO_Workshop
+{
+    public class ComposedProduct : Product
+    {
+        public float Discount { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+        public override decimal ValueToPay()
+        {
+            decimal valueToPay = 0;
+            foreach (Product product in Products)
+            {
+                valueToPay += product.ValueToPay();
+            }
+            return valueToPay - (valueToPay * (decimal)Discount);
+        }
+        public override string ToString()
+        {
+            string products = string.Empty;
+            foreach (Product product in Products)
+            {
+                products += product.Description + ", ";
+            }
+            products = products.Remove(products.Length - 2, 2);
+            return $"{Id} {Description}" +
+                $"\n\tProducts.......: {$"{products}",20}" +
+                $"\n\tDiscount.......: {$"{Discount:P2}",20}" +
+                $"\n\tValue..........: {$"{ValueToPay():C2}",20}";
+        }
+    }
+}
---
+++ POO-Workshop/POO-Workshop/Invoice.cs
@@ -0,0 +1,38 @@
+﻿namespace POO_Workshop
+{
+    public class Invoice : IPay
+    {
+        private ICollection<Product> _products;
+
+        public Invoice()
+        {
+            _products = new List<Product>();
+        }
+        public void AddProduct(Product product)
+        {
+            _products.Add(product);
+        }
+        public decimal ValueToPay()
+        {
+            decimal valueToPay = 0M;
+            foreach(Product product in _products)
+            {
+                valueToPay += product.ValueToPay();
+            }
+            return valueToPay;
+        }
+        public override string ToString()
+        {
+            string products = string.Empty;
+            foreach (Product product in _products)
+            {
+                products += product.ToString()+"\n";
+            }
+            return $"RECEIPT" +
+                $"\n---------------------------------------------" +
+                $"\n{products}" +
+                $"                        ====================="+
+                $"\nTOTAL                    {$"{ValueToPay():C2}",20}";
+        }
+    }
+}
